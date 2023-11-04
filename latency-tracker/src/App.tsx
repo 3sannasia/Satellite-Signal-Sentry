a02f555446847ea5797a890045767a49a8e6ad26
Akash Sannasi <sannasi2@illinois.edu>
2023-11-04 18:05:49 -0500

    Add access key env file

--- latency-tracker/src/App.tsx
+++ latency-tracker/src/App.tsx
@@ -2,9 +2,8 @@ import { Viewer, Entity } from "resium";
 import { Cartesian3, Color } from "cesium";
 import { Ion } from "cesium";
 
-// Ion.defaultAccessToken =
-//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIwMTZjNmFjZS0zYmZkLTQzOTYtOWY5YS0wYWEwNDdmNjBhN2EiLCJpZCI6MTc1OTk5LCJpYXQiOjE2OTkxMDk3NDN9.jzOPC9K-BKERkNyWyoWGW9PAwZMcIppuJCix1vdqC-I";
-Ion.defaultAccessToken = process.env.CESIUM_ACCESS_KEY || "hello";
+Ion.defaultAccessToken =
+  process.env.CESIUM_ACCESS_KEY || "Access Key Not Found";
 console.log("Ion.defaultAccessToken", Ion.defaultAccessToken);
 const position = Cartesian3.fromDegrees(-74.0707383, 40.7117244);
 const pointGraphics = { pixelSize: 20, color: Color.RED };
