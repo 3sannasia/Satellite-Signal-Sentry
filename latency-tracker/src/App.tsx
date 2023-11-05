de168dc2e55c9f3b6e130b272259bc4c7e542d35
Akash Sannasi <sannasi2@illinois.edu>
2023-11-04 19:02:24 -0500

    Add tech stack changes to project proposal

--- latency-tracker/src/App.tsx
+++ latency-tracker/src/App.tsx
@@ -4,6 +4,7 @@ import { Ion } from "cesium";
 
 Ion.defaultAccessToken =
   process.env.CESIUM_ACCESS_KEY || "Access Key Not Found";
+
 console.log("Ion.defaultAccessToken", Ion.defaultAccessToken);
 const position = Cartesian3.fromDegrees(-74.0707383, 40.7117244);
 const pointGraphics = { pixelSize: 20, color: Color.RED };
