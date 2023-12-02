324d4f1affa584ed58e8c6c8b55d433c42927dd4
Akash Sannasi <sannasi2@illinois.edu>
2023-12-02 15:12:52 -0600

    Add .gitignore and ignore __pycache__

--- pi_data.py
+++ pi_data.py
@@ -1,2 +1,32 @@
-# Pull data from pi and insert into database
-# Send data through nats to the frontend
+import gpsd
+import time
+
+def get_gps_info():
+    # Connect to the local GPSD service (assuming it's running on localhost)
+    gpsd.connect()
+    print(gpsd.device())
+    
+
+    # Get the GPSD data stream
+    packet = gpsd.get_current()
+    
+
+    # Print some basic information
+    print("Time (UTC):", packet.time)
+    print("Latitude:", packet.lat)  #requires mode >=2
+    print("Longitude:", packet.lon) #requires mode >=2
+    print("Altitude:", packet.alt) #requires mode >=3
+    print("Mode: " + str(packet.mode))
+    print("Satellites Seen: " + str(packet.sats))  
+ 
+
+if __name__ == "__main__":
+    try:
+        
+        while True:
+            get_gps_info()
+            print("\n---\n")
+            time.sleep(5)  # Wait for 5 seconds before getting the next update
+    except KeyboardInterrupt:
+        print("\nScript terminated by user.")
+
