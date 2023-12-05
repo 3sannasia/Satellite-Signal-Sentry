bb0a3fb5b57a2d7f15694750791a0e7b226f5d43
Akash Sannasi <sannasi2@illinois.edu>
2023-12-05 11:19:25 -0600

    Add pi database insert gps data

--- testing.py
+++ testing.py
@@ -10,6 +10,7 @@ import datetime
 #https://gpsd.gitlab.io/gpsd/gpsd_json.html#_sky
 # Data attributes and descriptions in link above
 
+gpsd = gps.gps(mode=gps.WATCH_ENABLE|gps.WATCH_NEWSTYLE)
 
 running = True
 uuid = str(uuid.uuid4())
@@ -34,45 +35,64 @@ def register_to_database():
     
     print("Device registered to database!")
     
+    
 def unregister_from_database():
     unregister_query = "DELETE FROM connected_devices WHERE uuid = ?"
-    cursor.execute(unregister_query, (uuid))
+    cursor.execute(unregister_query, (uuid,))
     connection.commit()
     
     print("Device unregistered from database!")
-register_to_database()
-unregister_from_database()
 
-def get_TPV_SKY_data(gps):
-    nx = gps.next()
+
     
+def get_TPV_SKY_device_data(gps):
+    nx = gps.next()
+    time = None
+    longitude = None
+    latitude = None
+    altitude = None
+    mode = None
+    tdop = None
+    nSat = None
+    uSat = None
+    satellites = None
+
     if nx['class'] == 'TPV':
         if hasattr(nx, 'time'):
             print("Time: ", nx.time)
+            time = nx.time
         if hasattr(nx, 'lon'):
             print("Longitude: ", nx.lon)
+            longitude = nx.lon
         if hasattr(nx, 'lat'):
             print("Latitude: ", nx.lat)
+            latitude = nx.lat
         if hasattr(nx, 'alt'):
             print("Altitude: ", nx.alt, 'm')
+            altitude = nx.alt
         if hasattr(nx, 'mode'):
+            mode = nx.mode
             print("Mode: ", nx.mode)
             
    
     if nx['class'] == 'SKY':
         if hasattr(nx, 'satellites'):
+            satellites = nx.satellites
             for satellite_info in nx.satellites:
                 print("Satellite: ", satellite_info)
                  
         if hasattr(nx, 'tdop'): #time dilution based on satellite position
             print("TDOP: ", nx.tdop)
+            tdop = nx.tdop
         if hasattr(nx, 'nSat'):
             print("Satellites Seen:", nx.nSat)
+            nSat = nx.nSat
         if hasattr(nx, 'uSat'):
+            uSat = nx.uSat
             print("Satellites Used:", nx.uSat)
-        
-    
-    
+    insert_GPS_data(time, longitude, latitude, altitude, mode, tdop, nSat, uSat, satellites, get_device_temperature(), get_cpu_frequency())
+ 
+
 def get_cpu_frequency():
     # Get CPU frequency in Hz
     frequency = psutil.cpu_freq().current
@@ -83,20 +103,74 @@ def get_device_temperature():
     cpu_temp = CPUTemperature().temperature
     return cpu_temp
 
-        
+
+def insert_GPS_data(time, longtitude, latitude, altitude, mode, tdop, nSat, uSat, satellites, cpu_temp, cpu_freq):
+    gps_json = {
+        'time': time,
+        'longitude': longitude,
+        'latitude': latitude,
+        'altitude': altitude,
+        'mode': mode,
+        'nSat': nSat,
+        'uSat': uSat,
+        'TDOP': tdop,
+        'satellites': satellites,
+        'cpu_temp': cpu_temp,
+        'cpu_freq': cpu_freq
+    }
+    gps_insert_query = "INSERT INTO gps_data (time, longitude, latitude, altitude, mode, nSat, uSat, TDOP, satellites, cpu_temp, cpu_freq) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+    cursor.execute(gps_insert_query, (gps_json['time'], gps_json['longitude'], gps_json['latitude'], gps_json['altitude'], gps_json['mode'], gps_json['nSat'], gps_json['uSat'], gps_json['TDOP'], gps_json['satellites'], gps_json['cpu_temp'], gps_json['cpu_freq']))
+    connection.commit()
+    print("GPS data inserted to database!")
+    
+    
+def print_TPV_SKY_data(gps):
+    nx = gps.next()
+    nx2 = gps.next()
+    
+    print(nx['class'])
+    print(nx2['class'])
+    # import pdb; pdb.set_trace()
+    # if nx['class'] == 'TPV':
+    #     if hasattr(nx, 'time'):
+    #         print("Time: ", nx.time)
+    #     if hasattr(nx, 'lon'):
+    #         print("Longitude: ", nx.lon)
+    #     if hasattr(nx, 'lat'):
+    #         print("Latitude: ", nx.lat)
+    #     if hasattr(nx, 'alt'):
+    #         print("Altitude: ", nx.alt, 'm')
+    #     if hasattr(nx, 'mode'):
+    #         print("Mode: ", nx.mode)
             
+   
+    # if nx['class'] == 'SKY':
+    #     if hasattr(nx, 'satellites'):
+    #         for satellite_info in nx.satellites:
+    #             print("Satellite: ", satellite_info)
+                 
+    #     if hasattr(nx, 'tdop'): #time dilution based on satellite position
+    #         print("TDOP: ", nx.tdop)
+    #     if hasattr(nx, 'nSat'):
+    #         print("Satellites Seen:", nx.nSat)
+    #     if hasattr(nx, 'uSat'):
+    #         print("Satellites Used:", nx.uSat)
+            
+    # print(str(get_cpu_frequency()) + " Hz")
+    # print(str(get_device_temperature()) + " C")
 
 
-# gpsd = gps.gps(mode=gps.WATCH_ENABLE|gps.WATCH_NEWSTYLE)
+try:
+    register_to_database()
+    print("GPS Application Started!")
+    while running:
+        print_TPV_SKY_data(gpsd)
 
-# try:
-#     print("Application started!")
-#     while running:
-#         get_TPV_SKY_data(gpsd)
-#         print(str(get_cpu_frequency()) + " Hz")
-#         print(str(get_device_temperature()) + " C")
-#         print("\n---\n")
-#         time.sleep(2)
-# except KeyboardInterrupt:
-#     running = False
-#     print("Applications closed!")
+        print("\n---\n")
+        time.sleep(2)
+except KeyboardInterrupt:
+    running = False
+    print('\n')
+    unregister_from_database()
+    
+    print("GPS closed!")
