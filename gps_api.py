0d0fffd2544af21f7e3282255477718fd5df67ad
Akash Sannasi <sannasi2@illinois.edu>
2023-12-05 18:08:33 -0600

    add fastapi routes and uvicorn server

--- gps_api.py
+++ gps_api.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
+import sqlite3
 
 app = FastAPI()
+# run with uvicorn gps_api:app --port 5001 --reload
+
+# Establish a connection to the SQLite database
+db_file = "gps_test.db"
+connection = sqlite3.connect(db_file)
+cursor = connection.cursor()
+
+@app.get("/")
+def read_root():
+    return {"API": "See swagger docs at /docs"}
+
 
 # Route to send latitude, location, and altitude data
 @app.post("/data")
 def send_data(latitude: float, location: str, altitude: float):
     # Process and store the data
+    
     # ...
     return {"message": "Data received successfully"}
 
@@ -23,3 +36,6 @@ def get_satellite_data(uuid: str):
     # ...
     return {"message": f"Satellite data for UUID: {uuid}"}
 
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=5000, log_level="info")
